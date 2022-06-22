694cd08a22083e6d6fa733da59173a33a472aacf
Abhinav Kumar <abhinavkumar34002@gmail.com>
2022-06-22 06:50:01 +0530

    Basic type declaration in Typescript

--- hello-ts/app.js
+++ hello-ts/app.js
@@ -1,1 +1,6 @@
-console.log("Hello TypeScrit");
+console.log("Namaste TypeScrit");
+function add(n1, n2) {
+    return n1 + n2;
+}
+var result = add(5, 8);
+console.log(result);
--- hello-ts/app.ts
+++ hello-ts/app.ts
@@ -1,1 +1,8 @@
-console.log("Hello TypeScrit")
+console.log("Namaste TypeScrit")
+
+function add(n1:number,n2:number) :number{
+    return n1+n2;
+}
+
+var result:number = add(5,8)
+console.log(result);
